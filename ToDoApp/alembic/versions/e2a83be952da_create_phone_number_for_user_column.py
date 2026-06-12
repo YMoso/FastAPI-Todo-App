73033a0e91053945bced604112906f1920f6fcd6
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-06-13 00:27:50 +0200

    feat: add user phone number update with Alembic migration

---
+++ ToDoApp/alembic/versions/e2a83be952da_create_phone_number_for_user_column.py
@@ -0,0 +1,27 @@
+"""Create phone number for user column
+
+Revision ID: e2a83be952da
+Revises: 
+Create Date: 2026-06-12 16:41:28.852163
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e2a83be952da'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('phone_number', sa.String(length=10), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users', 'phone_number')
+
--- ToDoApp/routers/admin.py
+++ ToDoApp/routers/admin.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
-from pydantic import BaseModel, Field
 from starlette import status
 from models import Todos
 from database import SessionLocal
