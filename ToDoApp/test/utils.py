9c67eaeed8d0dc2d65d5fc2f2d0e3ee29a801f84
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-06-25 01:02:43 +0200

    test: add API router test suite

---
+++ ToDoApp/test/utils.py
@@ -0,0 +1,70 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from ..database import Base
+from ..main import app
+from fastapi.testclient import TestClient
+import pytest
+from ..models import Todos, Users
+from ..routers.auth import bcrypt_contex
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False},
+                       poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def override_get_current_user(user: dict = None):
+    return {'username': 'dziabtest', 'id': 1, 'user_role': 'admin'}
+
+
+client = TestClient(app)
+
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title='learn to code',
+        description='learn to code in python',
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as conn:
+        conn.execute(text('DELETE FROM todos;'))
+        conn.commit()
+
+@pytest.fixture
+def test_user():
+    user = Users(
+        username='dziabtest',
+        email='dziabdziak@gmail.com',
+        first_name='dziabdziak',
+        last_name='dziabdziak',
+        hashed_password=bcrypt_contex.hash('testpassword'),
+        phone_number='12345678',
+        role='admin'
+    )
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as conn:
+        conn.execute(text("DELETE FROM users;"))
+        conn.commit()
