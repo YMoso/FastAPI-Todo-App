d631b8092fd43ad96ec1318ca68ae56290d849e1
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-06-25 19:50:10 +0200

    feat: add homepage route and template rendering

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/healty")
+app.mount("/static", StaticFiles(directory="ToDoApp/static"), name="static")
+
+templates = Jinja2Templates(directory="ToDoApp/templates")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "user": None})
+
+
+@app.get("/healthy")
 def healthy_check():
     return {"status": "healthy"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
