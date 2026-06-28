68887360cb0a821bd5a1c2b968b756050c3d8654
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-06-28 16:51:20 +0200

    chore: add Docker setup

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -1,15 +1,42 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+#
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#
+# # postgresql://postgres:test123#@localhost:5432/TodoApplicationDatabase
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+#
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+# Base = declarative_base()
+#
+#
+
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+load_dotenv()
 
-# postgresql://postgres:test123#@localhost:5432/TodoApplicationDatabase
+BASE_DIR = Path(__file__).resolve().parent
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"sqlite:///{BASE_DIR / 'todosapp.db'}"
+)
+
+engine_args = {}
+
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine_args["connect_args"] = {"check_same_thread": False}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, **engine_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
